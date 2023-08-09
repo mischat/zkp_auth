7bef8ff9fd7cdb30629e1e094c3bedf4d85cc12d
Mischa <mischa@mmt.me.uk>
2023-08-09 17:33:02 +0100

    This tests one of the functions

--- test/zkp_auth_utils_test.go
+++ test/zkp_auth_utils_test.go
@@ -8,12 +8,37 @@ import (
 )
 
 func TestCalculateExp(t *testing.T) {
-	g := big.NewInt(2)
-	x := big.NewInt(12345)
-	p := big.NewInt(67890)
-	expectedY := big.NewInt(1234)
+	// should pass
+	g := big.NewInt(4)
+	x := big.NewInt(6)
+	p := big.NewInt(23)
+	expectedY := big.NewInt(2)
 
 	y := zkutils.CalculateExp(g, x, p)
+
+	if y.Cmp(expectedY) != 0 {
+		t.Errorf("CalculateExp(%v, %v, %v) = %v, expected %v", g, x, p, y, expectedY)
+	}
+
+	// should pass
+	g = big.NewInt(3)
+	x = big.NewInt(300)
+	p = big.NewInt(10009)
+	expectedY = big.NewInt(6419)
+
+	y = zkutils.CalculateExp(g, x, p)
+
+	if y.Cmp(expectedY) != 0 {
+		t.Errorf("CalculateExp(%v, %v, %v) = %v, expected %v", g, x, p, y, expectedY)
+	}
+
+	// should pass
+	g = big.NewInt(2892)
+	x = big.NewInt(300)
+	p = big.NewInt(10009)
+	expectedY = big.NewInt(4984)
+
+	y = zkutils.CalculateExp(g, x, p)
 
 	if y.Cmp(expectedY) != 0 {
 		t.Errorf("CalculateExp(%v, %v, %v) = %v, expected %v", g, x, p, y, expectedY)
