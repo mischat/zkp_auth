fe5f9b044b51b323fed0534324fe4b01cf457d5c
Mischa <mischa@mmt.me.uk>
2023-08-09 17:03:00 +0100

    This renames things internally

--- test/zkp_auth_utils_test.go
+++ test/zkp_auth_utils_test.go
@@ -7,13 +7,13 @@ import (
 	zkutils "github.com/mischat/zkp_auth/utils"
 )
 
-func TestCalculateY(t *testing.T) {
+func TestCalculateExp(t *testing.T) {
 	g := big.NewInt(2)
 	x := big.NewInt(12345)
 	p := big.NewInt(67890)
 	expectedY := big.NewInt(1234)
 
-	y := zkutils.CalculateY(g, x, p)
+	y := zkutils.CalculateExp(g, x, p)
 
 	if y.Cmp(expectedY) != 0 {
 		t.Errorf("CalculateY(%v, %v, %v) = %v, expected %v", g, x, p, y, expectedY)
